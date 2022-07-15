a1511f775d187f99899e443ae01d9cbeeb89ff93
NKC <khacchang128@gmail.com>
2022-07-15 11:56:27 +0700

    static file

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("UNIQLO SHOP API");
